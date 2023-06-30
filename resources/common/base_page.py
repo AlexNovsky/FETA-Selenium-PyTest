b8c6512f0e70c413b56acc90ef15a4dd39d245f1
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2023-06-29 22:42:58 -0500

    Initial commit.

---
+++ resources/common/base_page.py
@@ -0,0 +1,118 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+    """Base class for every page object of the web application under test
+    Includes all basic page methods, applicable to every page, like (is_displayed,
+    click, enter_text, get_title, and many more)
+    """
+
+    def __init__(self, driver) -> None:
+        """Initialize self
+        """
+        self.driver = driver
+
+    def is_displayed(self, locator: WebElement or str or tuple) -> bool:
+        """Check if an element with the provided locator is displayed or not
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                True if the element is displayed/visible
+                                False if the element is hidden or does not exist
+        """
+        try:
+            return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(locator))
+        except TimeoutException:
+            return False
+
+    def open_url(self, url: str) -> None:
+        """Open the provided url in a web browser
+
+        :param url:             String, to be typed into the text field
+        :return:                None
+        """
+        self.driver.get(url)
+
+    def click(self, locator: WebElement or str or tuple) -> None:
+        """Click action on provided element
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                None
+        """
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).click()
+
+    def scroll_to(self, locator: WebElement or str or tuple) -> None:
+        """Scroll to the provided element. From the nature of this command
+            bottom of the element will be at the bottom of the visible part
+            of the web page.
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                None
+        """
+        ActionChains(self.driver).scroll_to_element(locator).perform()
+
+    def hover_to(self, locator: WebElement or str or tuple) -> None:
+        """Hover over an element with the provided locator
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                None
+        """
+        ActionChains(self.driver).move_to_element(locator).perform()
+
+    def select_item_from_dropdown(self, dd_locator: WebElement or str or tuple, item_text: str):
+        """Select a drop-down item with the provided text from a drop-down menu with the provided locator
+
+        :param item_text:       String, representing the text contained in a drop-down item in a drop-down menu
+                                with the provided dd_locator
+        :param dd_locator:      One of: - WebElement object
+                                        - String Key to find a locator xpath string within page class's attributes
+                                        - 2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                None
+        """
+        Select(dd_locator).select_by_visible_text(item_text)
+
+    def is_clickable(self, locator: WebElement or str or tuple) -> bool:
+        """Check if an element with the provided locator is clickable or not
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                True if the element is displayed
+                                False if the element is hidden or does not exist
+        """
+        try:
+            return WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(locator))
+        except TimeoutException:
+            return False
+    def get_title(self):
+        """Return the page title of the currently active browser tab
+
+        :return:                String, representing the page title of the currently active browser tab
+        """
+        return self.driver.title
+
+    def fill(self, locator: WebElement or str or tuple, text: str) -> None:
+        """Typing provided text in the provided text field as login forms etc.
+
+        :param locator:         2-item tuple containing: (locator strategy, locator identifying string)
+        :param text:            String with the text, which will be sent to the provided text field
+        :return:                none
+        """
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).fill(text)
+
+    def clear(self, locator: WebElement or str or tuple) -> None:
+        """Clear a text field with the provided locator
+
+        :param locator:         One of: - WebElement object
+                                        - String Key to find a locator xpath string within page class's attributes
+                                        - 2-item tuple containing: (locator strategy, locator identifying string)
+        :return:                None
+        """
+        element = self.driver.find_element(locator).clear()
+        element.send_keys(Keys.COMMAND + "a")
+        element.send_keys(Keys.CONTROL + "a")
+        element.send_keys(Keys.DELETE)
+        element.send_keys(Keys.BACKSPACE)
---
+++ resources/disney/pages/application.py
@@ -0,0 +1,17 @@
+from resources.common.base_page import BasePage
+# from resources.disney.pages.home_page import HomePage
+# from resources.disney.pages.sign_in_page import SignInPage
+
+
+class Application:
+    """Returns an object, containing all the Page Objects of the web application under test.
+
+    Includes BasePage with all its standard methods (get_element, is_displayed, click, etc.)
+    and all the child page objects (HomePage etc.) with all their locators (header, search_bar,
+    product_name, footer, etc.)
+    """
+
+    def __init__(self, driver):
+        self.base_page = BasePage(driver)
+        # self.home_page = HomePage(driver)
+        # self.signin_page = SignInPage(driver)
